29ab423e412c166f17f23183c18d961adbf77564
asiekmurek <aaaa@o2.pl>
2020-06-26 22:19:16 +0200

    Add to Query type Float, Boolean and array of Booleans

--- index.js
+++ index.js
@@ -6,18 +6,34 @@ const typeDefs = gql`
         interestingUrls: [String]
         randomDiceThrow: Int
         fewNumbers: [Int]
+        pi: Float
+        isTodayFriday: Boolean
+        randomCoinTossesUntilTrue: [Boolean]
     }
 `;
-function rootValue(){ 
+const rootValue = () => { 
     const getRandomDiceThrow = sides => Math.ceil(Math.random() * sides);
+    const date = new Date();
+    const randomCoinToss = () => Math.random() >= 0.5; 
+    const getRandomCoinTossesUntilTrue = () => {
+        const result = [];
+        do {
+            result.push(randomCoinToss());
+        } while(!result[result.length-1]);
+        return result;
+    } 
+
     const data = {
         greeting: "Hello world",
         interestingUrls: ["https://kursreacta.pl", "https://64bites.com"],
         randomDiceThrow: getRandomDiceThrow(6),
-        fewNumbers: [1,2,3]
-    }
+        fewNumbers: [1,2,3], 
+        pi: Math.PI,
+        isTodayFriday: date.getDay() === 5,
+        randomCoinTossesUntilTrue: getRandomCoinTossesUntilTrue()
+    };
     return data;
-}
+};
 
 const server = new ApolloServer({ 
     typeDefs,
